4932cc4d668e9361ca2acedd876b5eb5ffea62c1
mhayesCS <mahayes21@students.desu.edu>
2023-11-20 04:37:47 +0000

    made changes

--- lab3.c
+++ lab3.c
@@ -7,10 +7,20 @@ extern int** sudoku_board;
 int* worker_validation;
 
 int** read_board_from_file(char* filename){
-    FILE *fp = NULL;
-    int** board = NULL;
 
-    // replace this comment with your code
+    FILE *fp = fopen(filename,"r");
+    int** board = (int**)malloc(ROW_SIZE * sizeof(int*));
+
+    for(int i = 0; i < COL_SIZE; i++)
+        board[i] = (int*) malloc(COL_SIZE * sizeof(int));
+
+    if(fp == NULL) return;
+
+
+    for(int r = 0; r < ROW_SIZE; r++)
+        for(int c = 0; c < COL_SIZE; c++)
+            fscanf(fp,"%d",&board[r][c]);
+    fclose(fp);
 
     return board;
 }
@@ -20,6 +30,11 @@ int is_board_valid(){
     pthread_t* tid;  /* the thread identifiers */
     pthread_attr_t attr;
     param_struct* parameter;
+
+    tid = (pthread_t*) malloc(sizeof(int*) * NUM_OF_THREADS);
+    parameter = (param_struct*) malloc(sizeof(param_struct) * NUM_OF_THREADS);
+
+    
     
     // replace this comment with your code
     
