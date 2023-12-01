9fef90571a83f0ab77e1409ede88e4dc2f367326
mhayesCS <mahayes21@students.desu.edu>
2023-12-01 04:02:23 +0000

    completed

--- lab3.c
+++ lab3.c
@@ -27,6 +27,28 @@ int** read_board_from_file(char* filename){
 
 }
 
+void* validate(void* arg){
+    param_struct* param = (param_struct*) arg;
+    int id = param->id;
+    int startrow = param->starting_row;
+    int endrow = param->ending_row;
+    int startcol = param->starting_col;
+    int endcol = param->ending_col;
+    int arr[9] = {0,0,0,0,0,0,0,0,0};
+    int current;
+
+    worker_validation[id] = 1;
+    
+    for(int r = startrow; r <= endrow; r++){
+        for(int c = startcol; c <= endcol; c++){
+            current = sudoku_board[r][c];
+            arr[current - 1] = 1;
+        }
+    }
+    for(int i = 0; i < 9; i++)
+        if(arr[i] != 1) worker_validation[id] = 0;
+}
+
 
 int is_board_valid(){
     pthread_t* tid;  /* the thread identifiers */
@@ -34,6 +56,60 @@ int is_board_valid(){
     tid = (pthread_t*) malloc(sizeof(int*) * NUM_OF_THREADS);
     param_struct* parameter = (param_struct*) malloc(sizeof(param_struct)* NUM_OF_THREADS);
     int index = 0;
+    worker_validation = (int*) malloc(sizeof(int) * 27);
+
+    for (int i = 0; i < ROW_SIZE; i++){
+        parameter[index].id = index;
+        parameter[index].starting_row = i;
+        parameter[index].starting_col = 0;
+        parameter[index].ending_row = i;
+        parameter[index].ending_col = COL_SIZE - 1;
+
+        pthread_create(&tid[index], NULL, validate, &parameter[index]);
+        index++;
+    }
+
+    for(int i = 0; i < COL_SIZE; i++){
+        parameter[index].id = index;
+        parameter[index].starting_row = 0;
+        parameter[index].starting_col = i;
+        parameter[index].ending_row = COL_SIZE - 1;
+        parameter[index].ending_col = i;
+
+        pthread_create(&tid[index], NULL, validate, &parameter[index]);
+        index++;
+    }
+
+    int r = 0, c = 0;
+    for(int i = 0; i < NUM_OF_SUBGRIDS; i++){
+        parameter[index].id = index;
+        parameter[index].starting_row = r;
+        parameter[index].starting_col = c;
+        parameter[index].ending_row = r + 2;
+        parameter[index].ending_col = c + 2;
+
+        pthread_create(&tid[index], NULL, validate, &parameter[index]);
+        index++;
+        if(c == 6){
+            r += 3;
+            c = 0;
+        } 
+        else c += 3;
+    }
+
+    
+    for(int i = 0; i< NUM_OF_THREADS; i++) 
+        pthread_join(tid[i], NULL);
+    
+    for (int x = 0; x< NUM_OF_THREADS; x++)
+        if(validation[x] != 1)return 0;
+
+    free(validation);
+    free(tid);
+    return 1;
+}
+
+
 
     
     
